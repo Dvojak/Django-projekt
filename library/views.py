6ef64a0835db5eef9ae8c7efeca20f2584dc3f2a
Dvojak <walter.obona@gmail.com>
2025-05-05 12:32:47 +0200

    Mushrooms

--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect ,get_object_or_404
 from .models import Deskovka, Zanr, Rozsireni
 from .forms import UserRegistrationForm, BoardModelForm
 from django.contrib.auth import login, authenticate, logout
@@ -43,8 +43,46 @@ class DeskovkaDeleteView(DeleteView):
     template_name = 'templates/games/boardgames_delete.html'
     context_object_name = 'deskovka'
     success_url = reverse_lazy('deskovka_list')
-    
-                
+
+def books_by_genre(request, genre):
+    if not Zanr.objects.filter(nazev=genre).exists():
+        return render(request, '404.html',status=404)
+    context = {
+        'genre': genre,
+        'deskovky': Deskovka.objects.order_by('nazev').filter(zanr__zanr=genre),
+    }
+    return render(request,'templates/games/boardgames_by_genre.html',context=context)
+
+def create_deskovka(request):
+    if request.method == 'POST':
+        form = BoardModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('deskovka_list')
+    else:   
+        form = BoardModelForm()
+
+    return render(request, 'templates/games/boardgames_create.html', {'form': form})
+        
+def upadate_deskovka(request, pk):
+    deskovka = get_object_or_404(Deskovka, pk=pk)
+    if request.method == 'POST':
+        form = BoardModelForm(request.POST, request.FILES, instance=deskovka)
+        if form.is_valid():
+            form.save()
+            return redirect('deskovka_detail', pk=deskovka.pk)
+    else:
+        form = BoardModelForm(instance=deskovka)
+
+    return render(request, 'templates/games/boardgames_update.html', {'form': form, 'deskovka': deskovka})
+
+def delete_deskovka(request, pk):
+    deskovka = get_object_or_404(Deskovka, pk=pk)
+    if request.method == 'POST':
+        deskovka.delete()
+        return redirect('deskovka_list')
+    return render(request, 'templates/games/boardgames_delete.html', {'deskovka': deskovka})
+
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
