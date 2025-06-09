b8f1bad1dfed50c433a998af03c6f317419bf1b3
Dvojak <walter.obona@gmail.com>
2025-06-09 23:26:24 +0200

    oneone_more

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Deskovka, Zanr, Rozsireni, Vydavatelstvi
+from .models import Deskovka, Zanr, Rozsireni, Vydavatelstvi, Tvurci, Role
 
 # Register your models here.
 admin.site.register(Deskovka)
 admin.site.register(Zanr)
 admin.site.register(Rozsireni)
 admin.site.register(Vydavatelstvi)
+admin.site.register(Tvurci)
+admin.site.register(Role)
