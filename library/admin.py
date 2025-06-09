2814903bbdbcf6e25d074e75cd5c2ea0497ac47b
Dvojak <walter.obona@gmail.com>
2025-06-09 23:19:53 +0200

    admin.py_changes

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Deskovka, Zanr, DeskovkaZanr, Rozsireni, Vydavatelstvi
 
 # Register your models here.
+admin.site.register(Deskovka)
+admin.site.register(Zanr)
+admin.site.register(DeskovkaZanr)
+admin.site.register(Rozsireni)
+admin.site.register(Vydavatelstvi)
