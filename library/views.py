1e3d4eaa122e730bdb620e38e2441c4ef5a921db
Dvojak <walter.obona@gmail.com>
2025-06-15 11:00:10 +0200

    login and admin work?

--- library/views.py
+++ library/views.py
@@ -220,30 +220,25 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.save()
+            user = form.save()  # jen jednou
             login(request, user)
             return redirect('dashboard')
     else:
         form = UserRegistrationForm()
-    context = {
-        'form': form,
-        }
-    return render(request, 'users/register.html', context=context)
+    return render(request, 'users/register.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
+
             
 def logout_view(request):
     if request.user.is_authenticated:
